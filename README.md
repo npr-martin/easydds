b0daf2b77318f559fd1eaed1519f2b0157818283
npr-martin <478439335@qq.com>
2024-12-18 19:32:56 +0800

    @edit change compiler function

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # easydds
 
 Project EasyDDS
+
+############配置环境#####
+cd /etc/ld.so.conf.d
+sudo touch easydds.conf
+sudo vim easydds.conf
+写入下方路径（绝对路径）
+[installPath]/install/lib
+[installPath]/easydds/build
+保存后执行
+sudo ldconfig
+
+使用./compiler.sh进行编译，可添加参数“-jn”进行多核编译，n为多核核数
--- compiler.sh
+++ compiler.sh
@@ -2,24 +2,39 @@
 mkdir foonathan_memory_vendor/build
 cd foonathan_memory_vendor/build
 cmake .. -DCMAKE_INSTALL_PREFIX=../../install -DBUILD_SHARED_LIBS=ON
-cmake --build . --target install
+cmake --build . --target install -- $1
 
 cd ../../
 
 mkdir Fast-CDR/build
 cd Fast-CDR/build
 cmake .. -DCMAKE_INSTALL_PREFIX=../../install
-cmake --build . --target install
+cmake --build . --target install -- $1
 
 cd ../../
 mkdir Fast-DDS/build
 cd Fast-DDS/build
 cmake ..  -DCMAKE_INSTALL_PREFIX=../../install
-cmake --build . --target install
+cmake --build . --target install -- $1
 
 cd ../../
 mkdir easydds/build
 cd easydds/build
 cmake ..  -DCMAKE_INSTALL_PREFIX=../../install
-make 
+cmake --build . --target install -- $1
+
+cd ../../
+mkdir Easy-DDS-Monitor/build
+cd Easy-DDS-Monitor/build
+qmake ../EDMonitor  
+make $1
+cp ./Easy-DDS-Monitor ../../install/bin
+
+
+cd ../../
+mkdir Easy-DDS-Test/build
+cd Easy-DDS-Test/build
+qmake ../EDTest  
+make $1
+cp ./EDTest ../../install/bin
 
